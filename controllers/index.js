4429c4d14cc3ae9dc5bd258f597abaf90f36417b
NikkiMounce <nikkimounce@gmail.com>
2021-09-28 15:05:32 -0700

    input home-route

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router(); //gives us a router
-const globalRoutes = require('./globalRoutes')
+const apiRoutes = require('./api');
+const globalRoutes = require('./globalRoutes');
+const dashRoutes = require('./dashboard-routes.js');
 
+router.use('/api', apiRoutes);
 router.use('/', globalRoutes);
+router.use('/dashboard', dashRoutes);
+router.use((req, res) => {
+    res.status(404).end();
+});
 
 module.exports = router;
