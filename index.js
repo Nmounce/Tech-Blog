f78711d6ccd5d58d9c6aef8ebc576848d15f6f91
NikkiMounce <nikkimounce@gmail.com>
2021-09-29 12:43:00 -0700

    finished...?

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-//associations
-const User = require('./User');
-const BlogPost = require('./BlogPost');
+// //associations
+// const User = require('./User');
+// const BlogPost = require('./BlogPost');
 
-User.hasMany(BlogPost, {
-    foreignKey: 'author',
-    onDelete: 'CASCADE'
-})
+// User.hasMany(BlogPost, {
+//     foreignKey: 'author',
+//     onDelete: 'CASCADE'
+// })
 
-BlogPost.belongsTo(User, {
-    foreignKey: 'author',
-})
+// BlogPost.belongsTo(User, {
+//     foreignKey: 'author',
+// })
 
-module.exports = {
-    User,
-    BlogPost
-};
+// module.exports = {
+//     User,
+//     BlogPost
+// //
